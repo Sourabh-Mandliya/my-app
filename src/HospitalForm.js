b58ea84fb02066775a4dc49efc00102ecbaf6e48
Sourabh-Mandliya <mandliyasourabh@gmail.com>
2021-01-11 11:46:46 +0530

    css remove

--- src/HospitalForm.js
+++ src/HospitalForm.js
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { Form, Input, InputGroup, Button, Label, FormGroup, Row, Col, Container,Table } from "reactstrap";
-
-import './HospitalForm.css';
+import { Form, Input, InputGroup, Button, Label, FormGroup, Row, Col, Container, Table } from "reactstrap";
 
 const HospitalForm = () => {
     return (
-        <Container style={{backgroundColor:"white",paddingTop:"2%",paddingLeft:"10%",paddingRight:"10%"}}>
-            <h5 style={{textAlign:"left"}}>Select Start Date and Shift Time</h5>
-            <p style={{textAlign:"left"}}>each row represents a shift</p>
+        <Container style={{ backgroundColor: "white", paddingTop: "2%", paddingLeft: "10%", paddingRight: "10%" }}>
+            <h5 style={{ textAlign: "left" }}>Select Start Date and Shift Time</h5>
+            <p style={{ textAlign: "left" }}>each row represents a shift</p>
             <br />
             <Form>
                 <FormGroup row>
                     <Label sm={3} md={3} lg={3} ><strong>Select Start Date</strong></Label>
-                    <Col sm={9}><Input type="date"></Input></Col>
+                    <Col sm={9}><Input type="date" required></Input></Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label sm={3} md={3} lg={3} ><strong>Select Repeat Type</strong></Label>
-                    <Col sm={9}>  
-                        <Input type="select">
+                    <Col sm={9}>
+                        <Input type="select" required>
                             <option value={'None'} >None</option>
                             <option value={'Daily'}>Daily</option>
                             <option value={"Weekly"}>Weekly</option>
@@ -26,45 +24,44 @@ const HospitalForm = () => {
                 </FormGroup>
                 <FormGroup row>
                     <Label sm={3} md={3} lg={3}><strong>Select Shift</strong></Label>
-                    <Col sm={9}>    
-                        <Input type="select">
-                            <option>None</option>
+                    <Col sm={9}>
+                        <Input type="select" required>
                             <option value="Morning Shift - 5am to 9am">Morning Shift - 5am to 9am</option>
-                            
+
                         </Input>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label sm={3} md={3} lg={3}><strong>Select Start Time</strong></Label>
-                    <Col sm={9}> <Input type="time"></Input></Col>
+                    <Col sm={9}> <Input type="time"  required></Input></Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label sm={3} md={3} lg={3} ><strong>Select End Time</strong></Label>
-                    <Col sm={9}>   <Input type="time"></Input></Col>
+                    <Col sm={9}> <Input type="time" required></Input></Col>
                 </FormGroup>
-                <div class="table-responsive" style={{ border: "1px solid black",marginTop:"2%",padding:"2%",position:"relative" }}>
-                <h5 style={{textAlign:"left",fontSize:"2vw"}}>Please select the day of the week</h5>
-                <br />
-                <Table>
-                    <thead>
-                        <th>MON</th>
-                        <th>TUE</th>
-                        <th>WED</th>
-                        <th>THU</th>
-                        <th>FRI</th>
-                        <th>SAT</th>
-                        <th>SUN</th>
-                    </thead>
-                    <tbody>
-                        <td ><Input type="radio" name="day"></Input></td>
-                        <td><Input type="radio" name="day"></Input></td>
-                        <td><Input type="radio" name="day"></Input></td>
-                        <td><Input type="radio" name="day"></Input></td>
-                        <td><Input type="radio" name="day"></Input></td>
-                        <td><Input type="radio" name="day"></Input></td>
-                        <td><Input type="radio" name="day"></Input></td>
-                    </tbody>
-                </Table>
+                <div class="table-responsive" style={{ border: "1px solid black", marginTop: "2%", padding: "2%", position: "relative" }}>
+                    <h5 style={{ textAlign: "left", fontSize: "2vw" }}>Please select the day of the week</h5>
+                    <br />
+                    <Table>
+                        <thead>
+                            <th>MON</th>
+                            <th>TUE</th>
+                            <th>WED</th>
+                            <th>THU</th>
+                            <th>FRI</th>
+                            <th>SAT</th>
+                            <th>SUN</th>
+                        </thead>
+                        <tbody>
+                            <td ><Input type="radio" name="day"></Input></td>
+                            <td><Input type="radio" name="day"></Input></td>
+                            <td><Input type="radio" name="day"></Input></td>
+                            <td><Input type="radio" name="day"></Input></td>
+                            <td><Input type="radio" name="day"></Input></td>
+                            <td><Input type="radio" name="day"></Input></td>
+                            <td><Input type="radio" name="day"></Input></td>
+                        </tbody>
+                    </Table>
                 </div>
                 <br />
                 <Row>
@@ -73,12 +70,12 @@ const HospitalForm = () => {
                         <Label><strong>Week Days Only</strong></Label>
                     </Col>
                 </Row>
-                <br/>
+                <br />
                 <Row>
                     <Col sm={9} md={9} lg={9} />
-                    <Col>  <Button color="primary" style={{borderRadius:"15px"}}><strong>ADD+</strong></Button> </Col></Row>
+                    <Col>  <Button color="primary" style={{ borderRadius: "15px" }}><strong>ADD+</strong></Button> </Col></Row>
             </Form>
-            <br/>
+            <br />
         </Container>
     )
 }
